f1eaf0a32bd5392d58de7fdac23040672f142512
Deepthi9390 <161426028+Deepthi9390@users.noreply.github.com>
2024-04-12 15:37:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import './App.css'
+import Movies from './Movies'
+import { useState } from 'react'
+
+function App ()
+{
+  const [searchTerm,setSearchTerm]=useState('')
+  const [moviesList, setMoviesList] = useState([])
+  
+  useEffect(() =>
+  {
+    async function fetchData(){
+      const res = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=e3277b8e`)
+      console.log(res)
+      const data = await res.json()
+      console.log(data)
+      setMoviesList(data.Search)
+    }
+    fetchData()
+  },[searchTerm])
+
+  return (
+    <div>
+      <h1>Movie Search</h1>
+      <input type="text" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} /><br></br>
+    <Movies moviesList={moviesList} />  
+   </div>
+  )
+}
+
+export default App
+
+
+
